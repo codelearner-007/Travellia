8e6c756aabcc88af386c81462ffa42590e2f2ba7
codelearner-007 <farhanahmad0819@gmail.com>
2025-12-24 23:47:03 +0500

    SummerDeals Styling

--- src/components/homepage/SummerDeals/index.jsx
+++ src/components/homepage/SummerDeals/index.jsx
@@ -23,7 +23,7 @@ const index = () => {
   ];
 
   return (
-    <section className="relative lg:min-h-[115vh] flex items-end justify-end">
+    <section className="relative h-[60vh] sm:min-h-[75vh] md:min-h-[75vh] lg:min-h-[115vh] flex items-end justify-end">
       <Image
         src={"/home/summer-deals/plane.png"}
         alt="plane"
@@ -32,24 +32,24 @@ const index = () => {
       />
       <ContentLayoutWrapper
         className={
-          "flex flex-col items-center justify-between gap-8 relative z-1 h-full py-72 text-center px-16"
+          "flex flex-col items-center justify-center gap-8 relative z-1 h-full py-72 text-center px-16"
         }
       >
-        <div className="flex flex-col items-center justify-between gap-3">
-          <h1 className="text-primary text-3xl md:text-5xl lg:text-7xl font-bold uppercase">
+        <div className="flex flex-col items-center justify-between gap-5 xl:gap-8">
+          <h1 className="text-primary text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold uppercase">
             Big Summer Deals
           </h1>
-          <h3 className="text-gray-700 font-normal text-2xl md:text-3xl lg:text-5xl uppercase">
+          <h3 className="text-gray-700 font-normal text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl uppercase">
             are on now
           </h3>
-          <p className="text-lg md:text-2xl lg:text-3xl text-gray-500 uppercase">
+          <p className="text-lg md:text-2xl lg:text-3xl xl:text-4xl text-gray-500 uppercase">
             Make your big summer gateaway happen: Members save 25% or more on
             thousands of hotels worldwide.
           </p>
         </div>
         <Button className={"px-20 py-8 text-2xl rounded-3xl"}>Load More</Button>
-        <div className="py-8 px-8 sm:p-16 mx-5 sm:mx-0  rounded-3xl shadow-2xl flex items-center justify-center gap-6 bg-gray-200 w-[90%] sm:w-full absolute -bottom-40">
-          <div className="flex flex-col md:flex-row items-center gap-8 justify-between md:w-full">
+        <div className="py-8 px-8 sm:p-10 lg:p-16 mx-5 sm:mx-0  rounded-3xl shadow-2xl flex items-center justify-center gap-6 bg-gray-200 w-[90%] sm:w-full absolute -bottom-30 sm:-bottom-40">
+          <div className="flex md:flex-row items-center gap-3 sm:gap-8 justify-between md:w-full">
             {STATS.map((stat, index) => (
               <React.Fragment key={index}>
                 <div className="flex flex-col items-center text-center ">
@@ -59,18 +59,18 @@ const index = () => {
                       alt={stat.label}
                       width={80}
                       height={80}
-                      className="w-20 h-20 object-contain"
+                      className="w-10 sm:w-20 h-10 sm:h-20 object-contain"
                     />
                   </div>
-                  <h2 className="text-5xl md:text-4xl lg:text-5xl font-bold text-gray-900 mb-3">
+                  <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-gray-900 mb-3">
                     {stat.number}
                   </h2>
-                  <p className="text-gray-700 font-bold text-xl tracking-wider uppercase">
+                  <p className="text-gray-700 font-bold text-xs sm:text-xl tracking-wider uppercase">
                     {stat.label}
                   </p>
                 </div>
                 {index !== STATS.length - 1 && (
-                  <div className="bg-gray-400/50 h-px w-3/10 rotate-180 md:rotate-90" />
+                  <div className="hidden sm:block sm:bg-gray-600/50 h-px w-3/10 rotate-180 md:rotate-90" />
                 )}
               </React.Fragment>
             ))}
