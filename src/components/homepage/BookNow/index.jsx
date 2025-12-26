08ced8239d7ecb2c8db4be178ff6801717c31cde
codelearner-007 <farhanahmad0819@gmail.com>
2025-12-26 12:41:50 +0500

    BookNow index styling

--- src/components/homepage/BookNow/index.jsx
+++ src/components/homepage/BookNow/index.jsx
@@ -3,23 +3,24 @@ import React from "react";
 
 const index = () => {
   return (
-    <section className="flex flex-col-reverse lg:grid lg:grid-cols-20 lg:h-[70vh]">
-      <div className="col-start-1 col-end-12 row-start-1 bg-primary rounded-t-[50px] lg:rounded-t-none lg:rounded-br-[100px] lg:rounded-tr-[100px] z-50 flex items-center -mt-10 lg:mt-0 py-12 lg:py-0 relative">
-        <div className="flex items-center justify-center lg:justify-end h-full w-full lg:mr-20 px-6 md:px-12 lg:px-0">
+    <section className="flex flex-col-reverse lg:grid lg:grid-cols-20 lg:min-h-[55vh] lg:max-h-[60vh] xl:h-[65vh] xl:pt-14">
+      <div className="col-start-1 col-end-12 row-start-1 bg-primary rounded-t-[50px] lg:rounded-t-none lg:rounded-br-[100px] lg:rounded-tr-[100px]  z-50 flex items-center -mt-10 lg:mt-0 py-12 lg:py-0 relative">
+        <div className="flex items-center justify-center  h-full w-full lg:mr-10 px-6 md:px-12 lg:px-0 ">
           <div className="flex flex-col gap-4 text-center lg:text-left items-center lg:items-start">
-            <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold capitalize text-gray-200 pb-3">
+            <h1 className="text-3xl lg:text-5xl xl:text-7xl font-bold capitalize text-gray-200 pb-3">
               Book now,
               <br />
               pay later
             </h1>
-            <p className="text-lg md:text-xl lg:text-2xl font-bold text-gray-200 max-w-[600px]">
+            <p className="text-lg lg:text-xl xl:text-2xl font-bold text-gray-200 max-w-[600px]">
               Book Flights Now, Pay Later. Lock-in today's price
-              <br className="hidden md:block" />& pay in up to 26 weekly payments.
+              <br className="hidden md:block" />& pay in up to 26 weekly
+              payments.
             </p>
-            <p className="text-lg md:text-xl lg:text-2xl font-bold text-gray-200">
+            <p className="text-lg lg:text-xl xl:text-2xl font-bold text-gray-200">
               No interest, no credit checks & no hidden fees.
             </p>
-            <p className="text-lg md:text-xl lg:text-2xl font-bold text-gray-200">
+            <p className="text-lg lg:text-xl xl:text-2xl font-bold text-gray-200">
               Book your next trip
             </p>
             <button className="bg-gray-100 text-primary px-12 md:px-24 py-4 rounded-3xl text-xl font-semibold w-fit mt-7 hover:bg-gray-50 transition-colors">
