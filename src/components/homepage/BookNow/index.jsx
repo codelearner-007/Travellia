ea66e7b59851da581b7b9681ab9dcb78d0f5783d
codelearner-007 <farhanahmad0819@gmail.com>
2025-12-26 13:07:56 +0500

    Book now index styling

--- src/components/homepage/BookNow/index.jsx
+++ src/components/homepage/BookNow/index.jsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const index = () => {
   return (
-    <section className="flex flex-col-reverse lg:grid lg:grid-cols-20 lg:min-h-[55vh] lg:max-h-[60vh] xl:h-[65vh] xl:pt-14">
-      <div className="col-start-1 col-end-12 row-start-1 bg-primary rounded-t-[50px] lg:rounded-t-none lg:rounded-br-[100px] lg:rounded-tr-[100px]  z-50 flex items-center -mt-10 lg:mt-0 py-12 lg:py-0 relative">
+    <section className="flex flex-col-reverse lg:grid lg:grid-cols-20 lg:min-h-[50vh] lg:max-h-[60vh] xl:min-h-[80vh] xl:pt-14 xl:gap-10">
+      <div className="col-start-1 col-end-12 row-start-1 bg-primary rounded-t-[50px] lg:rounded-t-none lg:rounded-br-[100px] lg:rounded-tr-[100px]  z-50 flex items-center -mt-10 lg:mt-0 py-12 lg:py-10 xl:py0 relative">
         <div className="flex items-center justify-center  h-full w-full lg:mr-10 px-6 md:px-12 lg:px-0 ">
-          <div className="flex flex-col gap-4 text-center lg:text-left items-center lg:items-start">
+          <div className="flex flex-col gap-4 xl:gap-10 text-center lg:text-left items-center lg:items-start">
             <h1 className="text-3xl lg:text-5xl xl:text-7xl font-bold capitalize text-gray-200 pb-3">
               Book now,
               <br />
@@ -29,7 +29,7 @@ const index = () => {
           </div>
         </div>
       </div>
-      <div className="h-[50vh] lg:h-full w-full col-start-10 col-end-21 row-start-1 relative">
+      <div className="h-[40vh] sm:h-[50vh] lg:h-full w-full col-start-10 col-end-21 row-start-1 relative">
         <Image
           src="/home/book-now/girl-on-island.png"
           alt="girl on island"
