a9484463d7c1ea5f939ee459528769e7395275ca
codelearner-007 <farhanahmad0819@gmail.com>
2026-01-01 15:19:22 +0500

    feat: add holidayPackages page with Welcome component

---
+++ src/app/holidayPackages/page.js
@@ -0,0 +1,12 @@
+import Welcome from "@/components/holidaypackagepage/Welcome";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex flex-col bg-background">
+      <Welcome />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/holidaypackagepage/Welcome.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import React from "react";
+import CarouselWrapper from "../ui/carousel";
+import ContentLayoutWrapper from "../common/ContentLayoutWrapper";
+
+const Welcome = () => {
+  const slides = [
+    { id: 1, image: "/holidayPackage/welcome/welcome1.jpg" },
+    { id: 2, image: "/holidayPackage/welcome/welcome2.jpg" },
+  ];
+
+  return (
+    <section className="h-[65vh] md:h-[72vh] relative lg:h-[77vh] xl:h-screen max-w-[100vw] flex items-center justify-center ">
+      <CarouselWrapper
+        slides={slides}
+        className={"!absolute"}
+        navigation={true}
+      />
+      <ContentLayoutWrapper className="flex flex-col items-center justify-center gap-7 z-10">
+        <h1
+          style={{ textShadow: "1px 1px 20px black" }}
+          className="font-semibold  text-white text-4xl sm:-translate-y-10 sm:text-5xl lg:-translate-y-20 lg:text-6xl xl:-translate-y-25 xl:text-7xl"
+        >
+          HOLIDAY PACKAGES
+        </h1>
+      </ContentLayoutWrapper>
+    </section>
+  );
+};
+
+export default Welcome;
